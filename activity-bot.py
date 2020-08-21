e4e371ead92fb918ddf1717ac9690e53ec5ad43a
alphonse295 <alphonse.raynaud@icloud.com>
2020-08-21 21:18:13 +0200

    changed a lot of things

--- activity-bot.py
+++ activity-bot.py
@@ -1,23 +1,25 @@
 import discord
 from discord.utils import get
-from dotenv import load_dotenv
+
+# from dotenv import load_dotenv
 
 client = discord.Client()
 users = {}
+Guild = client.get_guild(708982504690024490)
 
-somewhat_active = get(member.guild.roles, name="Somewhat Active")
-active = get(member.guild.roles, name="Active")
-very_active = get(member.guild.roles, name="very active")
-
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
-self.client = discord.Client()
+# load_dotenv()
+# TOKEN = os.getenv('DISCORD_TOKEN')
+# self.client = discord.Client()
+TOKEN = "token"
 
 
-def change_roles(member, counter):
+async def change_roles(member, counter):
+    somewhat_active = get(member.guild.roles, name="Somewhat Active")
+    active = get(member.guild.roles, name="Active")
+    very_active = get(member.guild.roles, name="very active")
 
     if counter >= 100 and counter < 500:
-        await member.add_role(somewhat_active)
+        await member.add_roles(somewhat_active)
     elif counter >= 500 and counter < 1000:
         if somewhat_active in member.roles:
             await member.remove_roles(somewhat_active)
@@ -30,19 +32,19 @@ def change_roles(member, counter):
 
 @client.event
 async def on_ready():
-        for channel in member.guild.text_channels:
-            async for message in channel.history:
-                if message.author == member:
-                    users[member]+=1
-        for members in client.guild.members:
-            change_roles(member, counter)
+
+    for channel in Guild.text_channels:
+        async for message in channel.history:
+            users[message.author] += 1
+    for member in client.guild.members:
+        change_roles(member, users[member])
 
     print("Done")
 
 
 async def on_message(message):
     users[message.author] += 1
-    change_roles(message.author, counter)
+    change_roles(message.author, users[message.author])
 
 
 client.run(TOKEN)
