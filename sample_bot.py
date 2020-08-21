745acba6cdbb0ee010beec2f8077a5593814d0f4
Nik3141 <33410313+Nik3141@users.noreply.github.com>
2020-08-21 12:45:26 -0700

    Update sample_bot.py

--- sample_bot.py
+++ sample_bot.py
@@ -21,13 +21,17 @@ class Bot:
             #setup
             self.guild = self.client.get_guild(746110570415128707)
             self.text_channels = self.guild.text_channels
-            self.members = self.guild.members
+            
+            #put non-bot members into the self.members list
+            self.members = []
+            for member in self.guild.members:
+                if member.bot = False:
+                    self.members.append(member)
             
             #create message count dictionary
             self.message_count = {}
             for member in self.members:
-                if member.bot = false:
-                    message_count[member] = 0
+                message_count[member] = 0
                 
             #activity roles
             self.activity_roles = {}
@@ -35,17 +39,17 @@ class Bot:
             self.activity_roles["active"] = get(self.guild.roles, name="Active")
             self.activity_roles["very_active"] = get(self.guild.roles, name="very active")
             
-            #remove previous activity roles
-            for member in self.members:
-                await member.remove_roles(self.activity_roles["somewhat_active"], self.activity_roles["active"], self.activity_roles["very_active"])
-            
             #count messages
             #WIP WIP WIP WIP WIP
             for channel in guild.text_channels:
                 async for message in channel.history(limit=None):
                     if message.author == client.user:
                         
-                        
+            
+            #remove previous activity roles
+            for member in self.members:
+                await member.remove_roles(self.activity_roles["somewhat_active"], self.activity_roles["active"], self.activity_roles["very_active"])
+            
             #assign roles based on number of messages
             for member in self.members:
                 if message_count[member] >= 100 and message_count[member] < 500:
