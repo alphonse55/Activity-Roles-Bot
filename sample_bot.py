31dd56d2dc4259c9925ceab452b1c10c4b0e3790
Nik3141 <33410313+Nik3141@users.noreply.github.com>
2020-08-21 14:13:58 -0700

    Update sample_bot.py

--- sample_bot.py
+++ sample_bot.py
@@ -25,13 +25,13 @@ class Bot:
             #put non-bot members into the self.members list
             self.members = []
             for member in self.guild.members:
-                if member.bot = False:
+                if member.bot == False:
                     self.members.append(member)
             
             #create message count dictionary
             self.message_count = {}
             for member in self.members:
-                message_count[member] = 0
+                self.message_count[member] = 0
                 
             #activity roles
             self.activity_roles = {}
@@ -41,28 +41,29 @@ class Bot:
             
             #count messages
             #WIP WIP WIP WIP WIP
-            for channel in guild.text_channels:
-                async for message in channel.history(limit=None):
-                    if message.author == client.user:
+            #for channel in guild.text_channels:
+                #async for message in channel.history(limit=None):
+                    #if message.author == client.user:
                         
             
             #remove previous activity roles
-            for member in self.members:
-                await member.remove_roles(self.activity_roles["somewhat_active"], self.activity_roles["active"], self.activity_roles["very_active"])
+            #for member in self.members:
+                #await member.remove_roles(self.activity_roles["somewhat_active"], self.activity_roles["active"], self.activity_roles["very_active"])
             
             #assign roles based on number of messages
-            for member in self.members:
-                if message_count[member] >= 100 and message_count[member] < 500:
-                    member.add_roles(self.activity_roles["somewhat_active"])
-                elif message_count[member] >= 500 and message_count[member] < 1000:
-                    member.add_roles(self.activity_roles["active"])
-                elif message_count[member] >= 1000:
-                    member.add_roles(self.activity_roles["very_active"])
+            #for member in self.members:
+                #if message_count[member] >= 100 and message_count[member] < 500:
+                    #member.add_roles(self.activity_roles["somewhat_active"])
+                #elif message_count[member] >= 500 and message_count[member] < 1000:
+                    #member.add_roles(self.activity_roles["active"])
+                #elif message_count[member] >= 1000:
+                    #member.add_roles(self.activity_roles["very_active"])
         
         #literally do nothing
         @self.client.event
         async def on_message(message):
             #message.channel.send()
+            pass
         
         #run the bot
         self.client.run(TOKEN)
