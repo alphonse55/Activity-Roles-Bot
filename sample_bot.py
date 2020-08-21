2cf69f92e51ab19f9a0c2d50675d78f233342f54
Nik3141 <33410313+Nik3141@users.noreply.github.com>
2020-08-21 11:35:06 -0700

    Update sample_bot.py

--- sample_bot.py
+++ sample_bot.py
@@ -1,4 +1,5 @@
 import discord, sys, os
+from discord.utils import get
 from dotenv import load_dotenv
 
 class Bot:
@@ -12,7 +13,21 @@ class Bot:
         @self.client.event
         async def on_ready():
             print(f"{self.client.user} has connected to Discord!")
-            self.guild = self.client.guilds[0]
+            
+            self.guild = self.client.get_guild(746110570415128707)
+            self.members = self.guild.members
+            
+            self.activity_roles = {}
+            self.activity_roles["somewhat_active"] = get(self.guild.roles, name="Somewhat Active")
+            self.activity_roles["active"] = get(self.guild.roles, name="Active")
+            self.activity_roles["very_active"] = get(self.guild.roles, name="very active")
+            
+            #iterate through members; first, remove all previous activity roles; second, traverse their message history and count their messages
+            for member in self.members:
+                await member.remove_roles(self.activity_roles["somewhat_active"], self.activity_roles["active"], self.activity_roles["very_active"])
+                
+                message_counter = 0
+                for message in member.history
         
         #literally do nothing
         @self.client.event
